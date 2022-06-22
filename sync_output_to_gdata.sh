0e1497f6889663f2bc53513ff65ec727754425be
Claire Yung <u6066740@anu.edu.au>
2022-06-22 17:15:50 +1000

    22 june 2022

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -3,14 +3,14 @@
 #PBS -l ncpus=1
 #PBS -l wd
 #PBS -l walltime=4:00:00,mem=4GB
-#PBS -P v45
+#PBS -P e14
 #PBS -N output_to_gdata
 #PBS -l storage=gdata/hh5+gdata/ik11+scratch/e14+gdata/ua8+gdata/e14
 
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # Make a unique path for your set of runs.
 # DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
-#GDATADIR=/g/data/e14/rmh561/access-om2/archive/025deg_jra55_ryf_norediGM/
+GDATADIR=/g/data/hh5/tmp/cy8964/access-om2/archive/025deg_jra55_ryf/
 
 mkdir -p ${GDATADIR}
 cd archive
--- sync_restarts_to_gdata.sh
+++ sync_restarts_to_gdata.sh
@@ -3,10 +3,10 @@
 #PBS -l ncpus=1
 #PBS -l wd
 #PBS -l walltime=2:00:00,mem=2GB
-#PBS -P v45
+#PBS -P e14
 #PBS -N restarts_to_gdata
 #PBS -l storage=gdata/hh5+gdata/ik11+scratch/e14+gdata/ua8+gdata/e14
 
-#source sync_output_to_gdata.sh # to define GDATADIR and cd archive
+source sync_output_to_gdata.sh # to define GDATADIR and cd archive
 
 rsync -vrltoD --safe-links restart* ${GDATADIR}
